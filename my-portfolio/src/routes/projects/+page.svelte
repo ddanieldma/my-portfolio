9ef91d3b0732b42129c1a2cebf1c359a4c92db5e
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2025-03-27 11:44:07 -0300

    init project and port pages

---
+++ my-portfolio/src/routes/projects/+page.svelte
@@ -0,0 +1,36 @@
+<h1>
+    Projects page
+</h1>
+
+<div class="projects">
+    <article>
+        <h3>Lorem ipsum dolor sit.</h3>
+        <img src="../images/project_image.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, quas quis accusantium laborum fugiat quasi sit officiis dignissimos rerum tenetur repellat necessitatibus? Porro quia voluptas voluptatem in amet cupiditate vero.</p>
+    </article>
+    <article>
+        <h3>Vel reiciendis ad praesentium!</h3>
+        <img src="../images/project_image.png" alt="">
+        <p>Doloremque magni optio perferendis quae autem eos possimus soluta! Similique quisquam provident ut nostrum, fugiat illo placeat vero consectetur praesentium incidunt distinctio a autem architecto commodi eligendi non deserunt libero.</p>
+    </article>
+    <article>
+        <h3>Sapiente dicta dignissimos cupiditate.</h3>
+        <img src="../images/project_image.png" alt="">
+        <p>Quas repudiandae ad perferendis id, est maxime sed? Itaque voluptatum totam exercitationem voluptatibus nihil accusantium consectetur dignissimos officia consequatur eligendi. Sint iure a voluptates voluptatum totam dolorem repellendus, incidunt quidem.</p>
+    </article>
+    <article>
+        <h3>Ipsam ratione saepe placeat.</h3>
+        <img src="../images/project_image.png" alt="">
+        <p>Aspernatur, quam fugit? Deserunt velit hic omnis, ad quidem pariatur neque laudantium dicta! Amet dignissimos minus officia labore vero molestiae numquam, cupiditate facilis tempore, ad, aspernatur ipsa necessitatibus ea culpa!</p>
+    </article>
+    <article>
+        <h3>Perferendis non dolorum mollitia?</h3>
+        <img src="../images/project_image.png" alt="">
+        <p>Illum accusamus animi accusantium! Pariatur tempora excepturi accusamus fugiat velit ipsum explicabo recusandae exercitationem quidem ducimus eius, sit minima vel deleniti natus repellat laudantium? Delectus repellat ea necessitatibus id quos.</p>
+    </article>
+    <article>
+        <h3>Veritatis temporibus adipisci fuga?</h3>
+        <img src="../images/project_image.png" alt="">
+        <p>Repellendus in ipsam magni possimus explicabo molestias dolore corporis dolorum natus distinctio assumenda ipsa rem, consequatur delectus praesentium reprehenderit veniam aliquam facere qui rerum nam quisquam. Doloremque voluptate dolores suscipit?</p>
+    </article>
+</div>
