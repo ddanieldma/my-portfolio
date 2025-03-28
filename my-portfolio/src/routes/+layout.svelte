399d7c2cf7f21329a7910ba73c8c74ca72362ab0
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2025-03-28 13:53:15 -0300

    tentando faze navbar fununcia

--- my-portfolio/src/routes/+layout.svelte
+++ my-portfolio/src/routes/+layout.svelte
@@ -6,12 +6,22 @@
         {url: "projects/", title: "Projects"},
         {url: "https://github.com/ddanieldma", title: "Projects"},
     ]
+
+    import { base } from "$app/paths";
+    import { page } from "$app/stores"
 </script>
 
 <nav>
-    {#each pages as page}
-        <a href="{page.url}">{page.title}</a>
+    {#each pages as page_info}
+        <a
+            href={page_info.url.startsWith("http") ? page_info.url: `${base}${page_info.url}`}
+            class:current={ $page.route.id === page_info.url }
+        >
+            {page_info.title}
+        </a>
     {/each}
 </nav>
 
 <slot />
+
+<!-- href="{page_info.url}" -->
