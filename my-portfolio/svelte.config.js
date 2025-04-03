a4af59c20d32f574ab104fd59fa9c5b0a80798f6
ddanieldma <danielalmemiranda@gmail.com>
2025-04-03 11:11:31 -0300

    configuring project for deploy

--- my-portfolio/svelte.config.js
+++ my-portfolio/svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+const repoName = "my-portfolio";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			base: dev ? '' : `/${repoName}`,
 		},
 	},
 };
