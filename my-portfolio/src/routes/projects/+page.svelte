d2ad16558a6af91bdbeccf7e4f717ef054c30375
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2025-04-03 12:08:33 -0300

    creates pie component

--- my-portfolio/src/routes/projects/+page.svelte
+++ my-portfolio/src/routes/projects/+page.svelte
@@ -5,11 +5,14 @@
 <script>
     import projects from "$lib/projects.json"
     import Project from "$lib/Project.svelte";
+    import Pie from "$lib/Pie.svelte";
 </script>
 
 <h1>
     { projects.length} Projects page
 </h1>
+
+<Pie />
 
 <div class="projects">
     {#each projects as p}
