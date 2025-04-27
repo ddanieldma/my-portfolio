5674021905031a91f13160ef41f3c925507e6acd
ddanieldma <danielalmemiranda@gmail.com>
2025-04-27 07:43:40 -0300

    adds the dots of the scatter plot

--- my-portfolio/src/routes/meta/+page.svelte
+++ my-portfolio/src/routes/meta/+page.svelte
@@ -20,43 +20,40 @@
             date: new Date(row.date + "T00:00" + row.timezone),
             datetime: new Date(row.datetime)
         }))
-
-        // --- Computing commits data ---
-        // First defining the array
-        let commits = []
-
-        // Grouping data by commit
-        commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
-            // Getting first line register of the commit
-            let first = lines[0]
-            // Defining the values based on this first line
-            let {author, date, time, timezone, datetime} = first
-            // Better structuring commit data
-            let ret = {
-                id: commit,
-                url: 'https://github.com/ddanieldma/my-portfolio/commit/' + commit,
-                author, date, time, timezone, datetime,
-                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-                totalLines: lines.length
-            }
-            
-            // Like ret.lines = lines
-            // but non-enumerable so it doesn't show up in JSON.stringify
-            Object.defineProperty(ret, "lines", {
-                value: lines,
-                configurable: true,
-                writable: true,
-                enumerable: false
-            })
-            
-            // Set loading to false to allow stats component to update
-            loading = false
-            return ret;
-        })
-
-        console.log("Commits data", commits)
     })
 
+    $: commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+        // Getting first line register of the commit
+        let first = lines[0]
+        // Defining the values based on this first line
+        let {author, date, time, timezone, datetime} = first
+        // Better structuring commit data
+        let ret = {
+            id: commit,
+            url: 'https://github.com/ddanieldma/my-portfolio/commit/' + commit,
+            author, date, time, timezone, datetime,
+            hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+            totalLines: lines.length
+        }
+        
+        // Like ret.lines = lines
+        // but non-enumerable so it doesn't show up in JSON.stringify
+        Object.defineProperty(ret, "lines", {
+            value: lines,
+            configurable: true,
+            writable: true,
+            enumerable: false
+        })
+        
+        // Set loading to false to allow stats component to update
+        loading = false
+
+        console.log(commits)
+
+        return ret;
+    })
+
+    // --- Setting up data for the stats component
     let statsLabels = [
         "Total Lines of Code",
         "Files",
@@ -69,17 +66,45 @@
         d3.groups(data, d => d.commit).length
     ]
 
+    // --- Scatter plot ---
+    // Dimensions of the coordinate space
+    let width = 1000, height = 600;
+
+    $: minDate = d3.min(commits.map(d => d.date))
+    $: maxDate = d3.max(commits.map(d => d.date))
+    $: maxDatePlusOne = new Date(maxDate)
+    $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1)
+
+    $: xScale = d3.scaleTime()
+        .domain([minDate, maxDatePlusOne])
+        .range([0, width])
+        .nice();
+
+    $: yScale = d3.scaleLinear()
+        .domain([24, 0])
+        .range([height, 0])
 </script>
 
 <h1>
     Meta
 </h1>
 
-Page about the stats of the source code of this project
+<h3>
+    Commits by time and day
+</h3>
 
-<p>
-    Total lines of code: {data.length}
-</p>
+<svg viewBox="0 0 {width} {height}">
+    <g class="dots">
+        {#each commits as commit, index}
+            <circle 
+                cx = { xScale(commit.datetime) }
+                cy = { yScale(commit.hourFrac) }
+                r="5"
+                fill="steelblue"
+            />
+        {/each}
+    </g>
+</svg>
 
 <Stats 
     sectionTitle="Summary"
@@ -88,3 +113,9 @@ Page about the stats of the source code of this project
     loading={loading}
     error={error}
 />
+
+<style>
+    svg {
+        overflow: visible;
+    }
+</style>
