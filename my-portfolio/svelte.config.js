088669ca372972cf6c93f514fbc98a9af2383ee3
ddanieldma <danielalmemiranda@gmail.com>
2025-05-22 12:09:18 -0300

    testing deploy

--- my-portfolio/svelte.config.js
+++ my-portfolio/svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
-const repoName = "my-portfolio";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
-
+		adapter: adapter({
+			fallback: 'index.html',
+			pages: 'build', // directory to output the static site
+			assets: 'build', // directory for static assets (should usually be the same as pages)
+		}),
 		paths: {
-			base: dev ? '' : `/${repoName}`,
+			base: process.argv.includes('dev') ? '' : '/my-portfolio',
 		},
 	},
 };
